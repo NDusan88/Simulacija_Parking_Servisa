c1ae36dc5ec83d35f9acef314821cf2bd17105fa
Dusan <dusan-nikolic88@hotmail.com>
2018-03-15 22:21:49 +0100

    Simulacija Parking Servisa

---
+++ WpfSimulacijaParkingServisa/Automobil.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSimulacijaParkingServisa
+{
+    class Automobil
+    {
+        private string registracija;
+        private string vlasnik;
+        private static int brojac = 0;
+
+        public Automobil()
+        {
+            brojac++;
+        }
+        public Automobil(string vlas, string reg)
+        {
+            brojac--;
+            registracija = reg;
+            vlasnik = vlas;
+        }
+        public void Upisi(string vlas, string reg)
+        {
+            registracija = reg;
+            vlasnik = vlas;
+        }
+
+        public string VratiReg()
+        {
+            return registracija;
+        }
+
+        public string VratiVlasnika()
+        {
+            return vlasnik;
+        }
+
+        public static int VratiBrojac()
+        {
+            return brojac;
+        }
+
+        public override string ToString()
+        {
+            return "\n\rRegistracija: " + registracija + "\n\r\n\rVlasnik: " + vlasnik;
+        }
+    }
+}
---
+++ WpfSimulacijaParkingServisa/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSimulacijaParkingServisa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private int kapacitet = 10;
+        private int slobodno;
+        private Automobil auto;
+
+        private void upis()
+        {
+            textBoxPrikaz.AppendText(auto.ToString());
+            textBoxPrikaz.AppendText("\n\r\n\rUkupno automobila: " + Automobil.VratiBrojac().ToString());
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBoxMesta.Text = kapacitet.ToString();
+            slobodno = kapacitet;
+        }
+
+        private void buttonUlaz_Click(object sender, RoutedEventArgs e)
+        {
+            if (slobodno > 0)
+            {
+                auto = new Automobil();
+                auto.Upisi(textBoxIme.Text, textBoxRegistracija.Text);
+                slobodno = kapacitet - Automobil.VratiBrojac();
+                textBoxMesta.Text = slobodno.ToString();
+
+                textBoxPrikaz.Clear();
+                textBoxPrikaz.Text = "Ulaz\n\r";
+                upis();
+            }
+            else
+            {
+                MessageBox.Show("Parking je pun");
+
+                textBoxRegistracija.Text = "";
+                textBoxIme.Text = "";
+                textBoxRegistracija.Focus();
+            }
+        }
+
+        private void buttonIzlaz_Click(object sender, RoutedEventArgs e)
+        {
+            if (slobodno < kapacitet)
+            {
+                auto = new Automobil(textBoxRegistracija.Text, textBoxIme.Text);
+
+                slobodno++;
+                textBoxMesta.Text = slobodno.ToString();
+
+                textBoxPrikaz.Clear();
+                textBoxPrikaz.Text = "Izlaz\n\r";
+                upis();
+            }
+            else
+            {
+                MessageBox.Show("Parking je prazan");
+            }
+        }
+    }
+}
+
